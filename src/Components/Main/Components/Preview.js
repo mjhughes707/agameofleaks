e5d12b9ae1e9441998f88f45cc931cc5a3b9c436
Matthew J. Hughes <mjhughes707@gmail.com>
2020-04-02 16:56:21 -0700

    began building Main component

---
+++ src/Components/Main/Components/Preview.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Preview.css";
+
+function Preview() {
+  return <div className="Preview">Image</div>;
+}
+
+export default Preview;
---
+++ src/Components/Main/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Main.css";
+import Preview from "./Components/Preview";
+
+function Main() {
+  return (
+    <main className="Main">
+      <Preview />
+      <Preview />
+    </main>
+  );
+}
+
+export default Main;
