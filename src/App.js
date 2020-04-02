f4af140a5b23be0a89c6f095cc55d90a16ae93ef
Matthew J. Hughes <mjhughes707@gmail.com>
2020-04-02 16:30:54 -0700

    began building Sidebar component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
+import Sidebar from "./Components/Sidebar/Sidebar";
 
 function App() {
   return (
     <div className="App">
       <Header className="header" />
       <Footer className="footer" />
+      <Sidebar className="sidebar" />
     </div>
   );
 }
---
+++ src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Sidebar.css";
+import Social from "./Components/Social";
+
+function Sidebar() {
+  return (
+    <div className="Sidebar">
+      <Social />
+    </div>
+  );
+}
+
+export default Sidebar;
