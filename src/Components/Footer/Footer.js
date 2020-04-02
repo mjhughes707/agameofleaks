2e1b4b51cd2847764ca1cfe45f66ff5661d99284
Matthew J. Hughes <mjhughes707@gmail.com>
2020-04-02 15:00:15 -0700

    built out beginning of Footer component

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="Footer">
+      <p className="footer-text">
+        Artwork by{" "}
+        <a
+          href="https://twitter.com/fawnilu_?lang=en"
+          target="_blank"
+          className="footer-link"
+        >
+          @fawnilu_
+        </a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
