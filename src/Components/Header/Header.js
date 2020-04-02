3111d66e302d1c2a53f31c4e03abf133432fb053
Matthew J. Hughes <mjhughes707@gmail.com>
2020-04-02 14:45:16 -0700

    built out beginning of Header component

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <header className="Header">
+      <h1 className="header-title">A Game of Leaks</h1>
+      <hr className="line-single" />
+      <p className="header-intro">
+        This project explores what Game of Thrones - Season 8{" "}
+        <span className="underline">might</span> have looked like, based off a
+        few rumored original outlines. The legitimacy of them is irrelevant.
+      </p>
+      <hr className="line-double" />
+    </header>
+  );
+}
+
+export default Header;
