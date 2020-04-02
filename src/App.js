eb100ee42504b4149a28b0436d62d43925c883b3
Matthew J. Hughes <mjhughes707@gmail.com>
2020-04-02 16:42:29 -0700

    resolved grid layout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./App.css";
 import Header from "./Components/Header/Header";
+import Main from "./Components/Main/Main";
 import Footer from "./Components/Footer/Footer";
 import Sidebar from "./Components/Sidebar/Sidebar";
 
 function App() {
   return (
     <div className="App">
-      <Header className="header" />
-      <Footer className="footer" />
-      <Sidebar className="sidebar" />
+      <Header />
+      <Main />
+      <Footer />
+      <Sidebar />
     </div>
   );
 }
