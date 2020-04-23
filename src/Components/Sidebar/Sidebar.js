59794110b658ab613a34cf23ee899559fa9e737c
Matthew J. Hughes <mjhughes707@gmail.com>
2020-04-23 11:15:50 -0700

    built out the sidebar component

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Sidebar.css";
 import Social from "./Components/Social";
+import Search from "./Components/Search";
+import Dropdown from "./Components/Dropdown";
+import Footer from "../Footer/Footer";
 
 function Sidebar() {
+  const [dropdown, setDropdown] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdown(!dropdown);
+  };
+
   return (
-    <div className="Sidebar">
-      <Social />
+    <div className="Sidebar hide-sm">
+      <div className="sidebar-top">
+        <Social />
+        <Search onClick={toggleDropdown} />
+        {dropdown && <Dropdown />}
+      </div>
+      <div className="sidebar-bottom">
+        <Footer />
+      </div>
     </div>
   );
 }
