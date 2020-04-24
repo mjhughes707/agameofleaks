b4c96dbae449ce2ec6844815c8c62f3adb9a327e
Matthew J. Hughes <mjhughes707@gmail.com>
2020-04-24 12:15:09 -0700

    updated click to also reset search field values

--- src/Components/Sidebar/Components/Search.js
+++ src/Components/Sidebar/Components/Search.js
@@ -7,10 +7,21 @@ import { faSearch } from "@fortawesome/free-solid-svg-icons";
 function Search() {
   const context = useContext(Context);
   const { get: dropdown, set: setDropdown } = context.dropdown;
+  const { set: setSearchValue } = context.searchValue;
+
+  // hides and displays search fields + resets values
+  const toggleDropdown = () => {
+    setDropdown(!dropdown);
+    setSearchValue({
+      Character: "",
+      Scene: "",
+      Page: "",
+    });
+  };
 
   return (
     <div className="Search">
-      <button className="search-button" onClick={() => setDropdown(!dropdown)}>
+      <button className="search-button" onClick={toggleDropdown}>
         <FontAwesomeIcon icon={faSearch} className="fa-2x" />
       </button>
     </div>
