ff2330f723bab3586141289cad84457bc5c25456
Matthew J. Hughes <mjhughes707@gmail.com>
2020-04-24 22:30:02 -0700

    hooked in context + reworked pagination + added useEffect

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect } from "react";
+import { Context } from "../../Context";
 import "./Main.css";
 import data from "../../data";
 import Preview from "./Components/Preview/Preview";
@@ -10,67 +11,113 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 function Main() {
-  const [currentPage, setCurrentPage] = useState(1);
-  const [pagination, setPagination] = useState(1);
-  const [modal, setModal] = useState(false);
+  const context = useContext(Context);
+  const { get: searchValue } = context.searchValue;
+  const {
+    get: prevIndexBegin,
+    set: setPrevIndexBegin,
+  } = context.prevIndexBegin;
+  const { get: prevIndexEnd, set: setPrevIndexEnd } = context.prevIndexEnd;
+  const { get: modal, set: setModal } = context.modal;
+  const { get: fullViewIndex, set: setFullViewIndex } = context.fullViewIndex;
 
-  const closeModal = e => {
+  // when search value updates, resets preview thumbnails to first two in array
+  useEffect(() => {
+    setPrevIndexBegin(0);
+    setPrevIndexEnd(2);
+  }, [searchValue, setPrevIndexBegin, setPrevIndexEnd]);
+
+  const closeModal = (e) => {
     e.target.id === "modal" && setModal(false);
   };
 
+  const prevModal = () => {
+    if (fullViewIndex === 1 && prevIndexBegin === 0) {
+      setFullViewIndex(0);
+    }
+  };
+
+  const prevPreview = () => {
+    if (prevIndexBegin > 0) {
+      setPrevIndexBegin(prevIndexBegin - 1);
+      setPrevIndexEnd(prevIndexEnd - 1);
+    }
+  };
+
   const prevPage = () => {
-    setCurrentPage(currentPage - 1);
-    setPagination(pagination - 1);
+    prevModal();
+    prevPreview();
+  };
+
+  const nextModal = () => {
+    if (fullViewIndex === 0 && prevIndexEnd === dataFiltered.length) {
+      setFullViewIndex(1);
+    }
+  };
+
+  const nextPreview = () => {
+    if (prevIndexEnd < dataFiltered.length) {
+      setPrevIndexBegin(prevIndexBegin + 1);
+      setPrevIndexEnd(prevIndexEnd + 1);
+    }
   };
 
   const nextPage = () => {
-    setCurrentPage(currentPage + 1);
-    setPagination(pagination + 1);
+    nextModal();
+    nextPreview();
   };
 
-  const activeFullView = data.Scenes.find(scene => scene.page === currentPage)
-    .src;
-
-  const activePreviews = data.Scenes.filter(
-    scene => scene.page === pagination || scene.page === pagination + 1
+  let dataFiltered = data.Scenes.filter(
+    (scene) =>
+      scene.characters.includes(searchValue.characters) ||
+      scene.scene === parseInt(searchValue.scene) ||
+      scene.page === parseInt(searchValue.page)
   );
+
+  if (dataFiltered.length === 0) {
+    dataFiltered = data.Scenes;
+  }
+
+  const activePreviews = dataFiltered.slice(prevIndexBegin, prevIndexEnd);
 
   return (
     <main className="Main">
       {modal && (
         <Modal
-          src={activeFullView}
-          currentPage={currentPage}
-          length={data.Scenes.length}
-          closeModal={e => closeModal(e)}
+          src={activePreviews[fullViewIndex].src}
+          length={dataFiltered.length}
+          closeModal={(e) => closeModal(e)}
           prevPage={prevPage}
           nextPage={nextPage}
         />
       )}
-      {pagination > 1 ? (
+
+      {prevIndexBegin > 0 ? (
         <FontAwesomeIcon
           icon={faArrowAltCircleLeft}
           className="font-awesome fa-2x"
-          onClick={prevPage}
+          onClick={prevPreview}
         />
       ) : (
         <div className="placeholder" />
       )}
-      {activePreviews.map(scene => (
+
+      {activePreviews.map((scene, i) => (
         <Preview
           key={scene.page}
-          scene={scene}
+          src={scene.src}
           onClick={() => {
-            setCurrentPage(scene.page);
+            setFullViewIndex(i);
             setModal(true);
           }}
         />
       ))}
-      {pagination < data.Scenes.length - 1 ? (
+
+      {prevIndexEnd < dataFiltered.length ? (
         <FontAwesomeIcon
           icon={faArrowAltCircleRight}
           className="font-awesome fa-2x"
-          onClick={nextPage}
+          onClick={nextPreview}
         />
       ) : (
         <div className="placeholder" />
