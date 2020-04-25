0045930d3b6737a74a8251a99ff0959393817cfd
Matthew J. Hughes <mjhughes707@gmail.com>
2020-04-24 22:24:40 -0700

    updated state in context api

--- src/Context.js
+++ src/Context.js
@@ -6,19 +6,23 @@ const { Provider } = Context;
 function ContextProvider(props) {
   const [dropdown, setDropdown] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [pagination, setPagination] = useState(1);
+  const [prevIndexBegin, setPrevIndexBegin] = useState(0);
+  const [prevIndexEnd, setPrevIndexEnd] = useState(2);
   const [modal, setModal] = useState(false);
+  const [fullViewIndex, setFullViewIndex] = useState(0);
   const [searchValue, setSearchValue] = useState({
-    Character: "",
-    Scene: "",
-    Page: "",
+    characters: "",
+    scene: "",
+    page: "",
   });
 
   const store = {
     dropdown: { get: dropdown, set: setDropdown },
     currentPage: { get: currentPage, set: setCurrentPage },
-    pagination: { get: pagination, set: setPagination },
+    prevIndexBegin: { get: prevIndexBegin, set: setPrevIndexBegin },
+    prevIndexEnd: { get: prevIndexEnd, set: setPrevIndexEnd },
     modal: { get: modal, set: setModal },
+    fullViewIndex: { get: fullViewIndex, set: setFullViewIndex },
     searchValue: { get: searchValue, set: setSearchValue },
   };
 
