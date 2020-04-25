73682954daf20b3c990db4c654c3087646febe1e
Matthew J. Hughes <mjhughes707@gmail.com>
2020-04-24 22:26:56 -0700

    removed search value reset when toggling dropdowns

--- src/Components/Sidebar/Components/Search.js
+++ src/Components/Sidebar/Components/Search.js
@@ -7,16 +7,10 @@ import { faSearch } from "@fortawesome/free-solid-svg-icons";
 function Search() {
   const context = useContext(Context);
   const { get: dropdown, set: setDropdown } = context.dropdown;
-  const { set: setSearchValue } = context.searchValue;
 
-  // hides and displays search fields + resets values
+  // hides and displays search fields
   const toggleDropdown = () => {
     setDropdown(!dropdown);
-    setSearchValue({
-      Character: "",
-      Scene: "",
-      Page: "",
-    });
   };
 
   return (
